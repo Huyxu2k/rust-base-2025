938503d90544bd2a995db2d137e0738c40b75286
huynv <huynv@phucthanhgroup.com>
2025-02-15 22:14:58 +0700

    token encode,decode

--- back-end/src/modules/auth/mod.rs
+++ back-end/src/modules/auth/mod.rs
@@ -1,6 +1,8 @@
 
 
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, TokenData, Validation};
 use serde::{Deserialize, Serialize};
+use crate::db_pool::DbPool;
 
 
 pub const REFRESH_EXPIRES: i64=24*60*60;
@@ -16,8 +18,8 @@ pub struct Token{
 #[derive(Debug,Clone,Deserialize,Serialize)]
 pub struct Claims{
     pub sub: String,
-    pub exp: usize,
-    pub iat: usize,
+    pub exp: i64,
+    pub iat: i64,
 }
 
 impl Claims {
@@ -28,5 +30,28 @@ impl Claims {
     }
 }
 
+pub fn encode_token(key:&str, claim:Claims)->Result<String,String>{
+    let token= encode(&Header::default(), &claim, &EncodingKey::from_secret(&key.as_ref()));
+    
+    match token {
+        Ok(tk) => Ok(tk),
+        Err(e) => {
+            Err(format!("Error encode token: {}",e))
+        }
+    }
+}
 
-pub fn encode()
+pub fn decode_token(token:String,key:&str)->Result<TokenData<Claims>,String>{
+   let result= decode::<Claims>(
+             &token,
+             &DecodingKey::from_secret(key.as_ref()),
+             &Validation::default(),//new(Algorithm::HS256)
+            );
+    match result {
+        Ok(de) => Ok(de),
+        Err(e) => Err(format!("Error decode token: {}",e)),
+    }
+}
+pub async fn verify_token(token_data:&TokenData<Claims>,pool: &DbPool)->Result<String,String>{
+    todo!()
+}
